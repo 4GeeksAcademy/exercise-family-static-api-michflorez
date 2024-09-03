78f07b20c901eada39947e81f880143e55b25b1e
Michflorez <167869106+Michflorez@users.noreply.github.com>
2024-09-03 12:32:52 +0000

    family actualizaciones

--- src/datastructures.py
+++ src/datastructures.py
@@ -1,3 +1,5 @@
+from random import randint
+
 class FamilyStructure:
     def __init__(self, last_name):
         self.last_name = last_name
@@ -27,17 +29,32 @@ class FamilyStructure:
         ]
 
     def _generate_id(self):
-        generated_id = self._next_id
-        self._next_id += 1
-        return generated_id
+      return randint(0,999999)
 
     def add_member(self, member):
+        current_members = self._members[:]
+
         if not all(key in member for key in ("first_name", "age", "lucky_numbers")):
             raise ValueError("Missing fields in member data")
         
-        member["id"] = self._generate_id()
+        if "id" not in member:  
+            member["id"] = self._generate_id()
+
         member["last_name"] = self.last_name
-        self._members.append(member)
+        member["id"] = int(member["id"])
+        member["age"] = int(member["age"])
+        member["lucky_numbers"] = list(member["lucky_numbers"])
+
+        new_member = {
+            "id": member["id"],
+            "first_name": member["first_name"],
+            "last_name": member["last_name"],
+            "age": member["age"],
+            "lucky_numbers": member["lucky_numbers"],
+        }    
+
+        current_members.append(new_member)
+        self._members = current_members
 
     def delete_member(self, id):
         self._members = [member for member in self._members if member["id"] != id]
