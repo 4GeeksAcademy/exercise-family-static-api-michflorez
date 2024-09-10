67d75467b48649eda5bf261ba680dc15cb88f040
Michflorez <167869106+Michflorez@users.noreply.github.com>
2024-09-10 12:44:22 +0000

    Cambios en post de member

--- src/app.py
+++ src/app.py
@@ -1,10 +1,24 @@
-from flask import Flask, request, jsonify
+import os
+from flask import Flask, request, jsonify, url_for
+from flask_cors import CORS
+from utils import APIException, generate_sitemap
 from datastructures import FamilyStructure
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
+CORS(app)
 
 jackson_family = FamilyStructure('Jackson')
 
+# Handle/serialize errors like a JSON object
+@app.errorhandler(APIException)
+def handle_invalid_usage(error):
+    return jsonify(error.to_dict()), error.status_code
+
+# generate sitemap with all your endpoints
+@app.route('/')
+def sitemap():
+    return generate_sitemap(app)
 
 initial_members = [
     {'first_name': 'John', 'age': 33, 'lucky_numbers': [7, 13, 22]},
@@ -39,12 +53,8 @@ def add_one_member():
     if not body or not all(k in body for k in ("first_name", "age", "lucky_numbers")):
         return jsonify({"error": "Invalid data"}), 400
 
-    new_member = {
-        "first_name": body['first_name'],
-        "age": body['age'],
-        "lucky_numbers": body['lucky_numbers']
-    }
-    jackson_family.add_member(new_member)
+    
+    new_member = jackson_family.add_member(body)
     return jsonify(new_member), 200
 
 @app.route('/member/<int:member_id>', methods=['DELETE'])
